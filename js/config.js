491e695107218dbca2b14ec256dc4546af79f049
tshaiman <tomer.shaiman@gmail.com>
2018-10-31 15:31:45 +0200

    serverless

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' //put the invoked URL from API Management 
+    }
+};
